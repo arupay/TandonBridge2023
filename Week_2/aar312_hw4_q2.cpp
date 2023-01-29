fc21ff2b5ab523d9d4a2607e4512164b62a5a116
arupay <arupay11374@gmail.com>
2023-01-29 10:00:16 -0500

    class problems

---
+++ Week_2/aar312_hw4_q2.cpp
@@ -0,0 +1,15 @@
+// I  V  X   L   C    D    M
+// 1  5  10  50  100  500  1000
+//  No Limit on M
+//  D, L, V at most once
+//  C X I at most 4 times
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  int userInput;
+  cout << "Enter a decimal number: " << endl;
+  cin >> userInput;
+}
