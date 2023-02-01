c96b8f914136f0d6c521ee1c80e02b7066bc9b7e
arupay <arupay11374@gmail.com>
2023-01-31 23:19:15 -0500

    completed all homework problems.

--- Week_2/aar312_hw4_q3.cpp
+++ Week_2/aar312_hw4_q3.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
-  int userInput;
-  string binaryRepresentation;
+  int userInput, binaryEquiv = 0, base = 1, remainder;
 
   cout << "Enter a decimal number: " << endl;
   cin >> userInput;
-  binaryRepresentation = "";
 
   int currNumber = userInput;
 
   while (currNumber > 0) {
     int remainder = currNumber % 2;
-    binaryRepresentation = to_string(remainder) + binaryRepresentation;
-    currNumber = currNumber / 2;
+    binaryEquiv = binaryEquiv + (remainder * base); // will effectly only add to binary if remainder > 0
+    base = base * 10;                               // increase base by * 10 every loop (every time currNum can be divided by 2)
+    currNumber = currNumber / 2;                    // update currNum
   }
 
-  cout << "The binary representation of " << userInput << " is " << binaryRepresentation << endl;
+  cout << "The binary representation of " << userInput << " is " << binaryEquiv << endl;
 
   return 0;
 }
