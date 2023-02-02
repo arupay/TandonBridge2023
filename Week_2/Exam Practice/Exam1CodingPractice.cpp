9b76336bb62f76493839cabb469cfd03ab39b27a
arupay <arupay11374@gmail.com>
2023-02-01 22:20:08 -0500

    finished all exam problems

--- Week_2/Exam Practice/Exam1CodingPractice.cpp
+++ Week_2/Exam Practice/Exam1CodingPractice.cpp
@@ -38,31 +38,99 @@ using namespace std;
 //   return 0;
 // }
 
-int main() {
-  int userInput, remainder, currEvaluating, sum = 0, innerProduct, digitFactorial;
+// int main() {
+//   int userInput, remainder, currEvaluating, sum = 0, innerProduct, digitFactorial;
 
-  cout << "Please enter a positive integer: " << endl;
-  cin >> userInput;
+//   cout << "Please enter a positive integer: " << endl;
+//   cin >> userInput;
 
-  currEvaluating = userInput;
+//   currEvaluating = userInput;
 
-  while (currEvaluating > 0) {
-    innerProduct = 1;
-    digitFactorial = currEvaluating % 10; // current digit to get factorial for
-    while (digitFactorial > 0) {
-      innerProduct *= digitFactorial;
-      digitFactorial--;
-    }
-    sum += innerProduct;
-    currEvaluating = currEvaluating / 10;
-  }
+//   while (currEvaluating > 0) {
+//     innerProduct = 1;
+//     digitFactorial = currEvaluating % 10; // current digit to get factorial for
+//     while (digitFactorial > 0) {
+//       innerProduct *= digitFactorial;
+//       digitFactorial--;
+//     }
+//     sum += innerProduct;
+//     currEvaluating = currEvaluating / 10;
+//   }
 
-  if (userInput == 0) {
-    cout << userInput << " is not a factorian" << endl;
-  } else if (userInput == sum) {
-    cout << userInput << " is a factorian" << endl;
-  } else {
-    cout << userInput << " is not a factorian" << endl;
-  }
-  return 0;
-}
+//   if (userInput == 0) {
+//     cout << userInput << " is not a factorian" << endl;
+//   } else if (userInput == sum) {
+//     cout << userInput << " is a factorian" << endl;
+//   } else {
+//     cout << userInput << " is not a factorian" << endl;
+//   }
+//   return 0;
+// }
+
+// int main() {
+
+// write  a program that reads a lower case inLetter, and prints a shape of an isosceles triangle, the first letter always lower case a,
+// up to the letter inputed alternating between lowercase and uppcase
+
+// example for input 'd'
+//_ _ _ a
+//_ _ B B B
+//_ c c c c c
+// D D D D D D D
+//   int LOWERCASE_START = 97;
+//   int UPPERCASE_START = 65;
+
+//   char inputLetter, space;
+//   int lineLimit, starLimit, spaceLimit, currentLetter;
+//   cout << "Please enter a lowercase letter" << endl;
+//   cin >> inputLetter;
+
+//   space = ' ';
+
+//   currentLetter = LOWERCASE_START;
+//   lineLimit = int(inputLetter + 1) - LOWERCASE_START; // +1 because i want to include that letter.
+
+//   spaceLimit = lineLimit - 1;
+//   starLimit = 1;
+
+//   for (int j = 0; j < lineLimit; j++) {
+//     for (int i = 0; i < spaceLimit; i++) {
+//       cout << space;
+//     }
+//     for (int i = 0; i < starLimit; i++) {
+//       cout << char(currentLetter);
+//     }
+//     spaceLimit--;
+//     starLimit += 2;
+//     j % 2 == 0 ? currentLetter -= 31 : currentLetter += 33;
+//     cout << endl;
+//   }
+// }
+
+// int main() {
+
+//   int ONE_FOOT = 12; // inches
+//   int ONE_YARD = 36; // inches
+
+//   int day = 1, sumInInches = 0, yards, feet, inches, userInput;
+
+//   do {
+//     cout << "Enter the number of inches the snail traveled on day #" << day << " , or type -1 if they reached their destination: " << endl;
+//     cin >> userInput;
+//     if (userInput > 0) {
+//       sumInInches += userInput;
+//       day++;
+//     }
+//   } while (userInput != -1);
+//   day = day - 1;
+
+//   yards = sumInInches / ONE_YARD;
+//   sumInInches %= ONE_YARD;
+//   feet = sumInInches / ONE_FOOT;
+//   sumInInches %= ONE_FOOT;
+//   inches = sumInInches;
+
+//   cout << "In " << day << " days, the snail traveled " << yards << " yards, " << feet << " feet and " << inches << " inches." << endl;
+
+//   return 0;
+// }
