03d0b6146a521fd30865084a6f02cb5c8c852f67
arupay <arupay11374@gmail.com>
2023-03-24 11:23:29 -0400

    complete

--- ExamPractice/EXAM2PracticeProbs.cpp
+++ ExamPractice/EXAM2PracticeProbs.cpp
@@ -462,9 +462,9 @@ using namespace std;
 //   return 0;
 // }
 
-int fib(int n) {
-  if (n <= 1) {
-    return n;
-  }
-  return fib(n - 1) + fib(n - 2);
-}
+// int fib(int n) {
+//   if (n <= 1) {
+//     return n;
+//   }
+//   return fib(n - 1) + fib(n - 2);
+// }
